0831a6bcfcaccdf8fd7aad7dbf38665cbcca5e05
IvyAndersen <ivyandandersen@gmail.com>
2025-12-04 16:15:56 +0100

    Update constants.js

---
+++ src/lib/constants.js
@@ -0,0 +1,11 @@
+// src/lib/constants.js
+export const MONTHS = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+export const YEARS = ['2024', '2025', '2026'];
+
+export const WEBHOOKS = {
+  CALCULATE_HOURS: 'https://primary-production-191cf.up.railway.app/webhook/Calculate_Hours',
+};
