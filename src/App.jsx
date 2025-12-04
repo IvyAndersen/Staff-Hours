2a6c718b6e21a0224b9efc89d9a7a8a7e3b0048d
IvyAndersen <ivyandandersen@gmail.com>
2025-12-04 16:01:43 +0100

    Update and rename App.jsx to src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+// src/App.jsx
+import React from 'react';
+import StaffDashboard from './components/StaffDashboard/StaffDashboard';
+
+export default function App() {
+  return <StaffDashboard />;
+}
